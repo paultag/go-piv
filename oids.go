81c1a33bae8a8b8c3ae8b4650dec20eb8e36cb21
Paul R. Tagliamonte <paultag@gmail.com>
2019-03-19 22:47:17 -0400

    Add in more OID comments.

--- oids.go
+++ oids.go
@@ -17,14 +17,28 @@ Medium Hardware, Medium Hardware-2048, Medium Hardware-112 and Medium Hardware-1
 PIV-Auth and PIV-Auth-2048. (Medium Hardware, but only used for physical)
 */
 
+// Information about the Key that belongs to the issued Certificate.
+// This contains information about the type of device that holds the
+// key, as well as the subscriber that that it was issued to.
 type Issued struct {
+	// This is true if the Key is used on a hardware device that does not
+	// allow the export of the private key material.
 	Hardware bool
-	Person   bool
+
+	// This is true if the Key is held by a Person, and False if it's held
+	// by a Non-Person Entity (NPE).
+	Person bool
 }
 
+// An expression of what CA Policy this Certificate was issued under.
 type Policy struct {
-	Name   string
-	Id     asn1.ObjectIdentifier
+	// Name of the Policy for humans to better understand.
+	Name string
+
+	// Identifier of the ASN.1 ObjectId of this Policy.
+	Id asn1.ObjectIdentifier
+
+	// Information about the key material and entity that holds it.
 	Issued Issued
 }
 
@@ -36,6 +50,7 @@ const (
 	// 	},
 	// }
 
+	// Medium assurance
 	DoDMediumNPE = Policy{
 		Name:       "DoDMediumNPE",
 		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 17},
@@ -66,6 +81,18 @@ const (
 		TokenClass: TokenClass{Person: true, Hardware: false},
 	}
 
+	DoDMedium112 = Policy{
+		Name:       "DoDMedium112",
+		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 39},
+		TokenClass: TokenClass{Person: true, Hardware: false},
+	}
+
+	DoDMedium128 = Policy{
+		Name:       "DoDMedium128",
+		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 40},
+		TokenClass: TokenClass{Person: true, Hardware: false},
+	}
+
 	DoDMediumHardware = Policy{
 		Name:       "DoDMediumHardware",
 		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 9},
@@ -90,47 +117,23 @@ const (
 		TokenClass: TokenClass{Person: true, Hardware: true},
 	}
 
-	// DoDPIVAuth = Policy{
-	// 	Name:       "DoDPIVAuth",
-	// 	Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 10},
-	// 	TokenClass: TokenClass{},
-	// }
+	DoDPIVAuth = Policy{
+		Name:       "DoDPIVAuth",
+		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 10},
+		TokenClass: TokenClass{Person: true, Hardware: true},
+	}
 
-	// DoDPIVAuth2048 = Policy{
-	// 	Name:       "DoDPIVAuth2048",
-	// 	Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 20},
-	// 	TokenClass: TokenClass{},
-	// }
+	DoDPIVAuth2048 = Policy{
+		Name:       "DoDPIVAuth2048",
+		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 20},
+		TokenClass: TokenClass{Person: true, Hardware: true},
+	}
 
 	// DoDPeerInterop = Policy{
 	// 	Name:       "DoDPeerInterop",
 	// 	Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 31},
 	// 	TokenClass: TokenClass{},
 	// }
-
-	DoDMedium112 = Policy{
-		Name:       "DoDMedium112",
-		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 39},
-		TokenClass: TokenClass{Person: true, Hardware: false},
-	}
-
-	DoDMedium128 = Policy{
-		Name:       "DoDMedium128",
-		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 40},
-		TokenClass: TokenClass{Person: true, Hardware: false},
-	}
-
-	DoDMediumHardware112 = Policy{
-		Name:       "DoDMediumHardware112",
-		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 42},
-		TokenClass: TokenClass{Person: true, Hardware: true},
-	}
-
-	DoDMediumHardware128 = Policy{
-		Name:       "DoDMediumHardware128",
-		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 43},
-		TokenClass: TokenClass{Person: true, Hardware: true},
-	}
 
 	DoDFORTEZZA = Policy{
 		Name:       "DoDFORTEZZA",
@@ -142,5 +145,13 @@ const (
 		Name:       "DoDType1",
 		Id:         asn1.ObjectIdentifier{2, 16, 840, 1, 101, 2, 1, 11, 6},
 		TokenClass: TokenClass{Person: false, Hardware: true},
+	}
+
+	allPolicies []Policy = []Policy{
+		DoDMediumNPE, DoDMediumNPE112, DoDMediumNPE128,
+		DoDMedium, DoDMedium2048, DoDMedium112, DoDMedium128,
+		DoDMediumHardware, DoDMediumHardware2048, DoDMediumHardware112, DoDMediumHardware128,
+		DoDFORTEZZA,
+		DoDType1,
 	}
 )
